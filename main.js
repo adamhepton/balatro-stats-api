ca0f40f5a65d46d82cb3665565b0a807ed36c7d0
Adam Hepton <adam@hepton.org>
2025-05-12 18:09:46 +0100

    Actually use direction

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import balatroStats from "#src/lib/balatroStats.js";
 import p from "#data/profile.json"  with { type: "json" };
 
 const [ type, query, direction ] = process.argv.slice(2, 5) ?? ["jokers", "count", "descending"];
-console.log(balatroStats[type].call(this, p, query));
+console.log(balatroStats[type].call(this, p, query, direction));
