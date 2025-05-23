ec87ebb78b6f3bc2da8674109fa12d2da537578e
Adam Hepton <adam@hepton.org>
2025-05-23 12:18:51 +0100

    Renamed direction to variant to more generically describe intent

--- src/lib/types/generic.js
+++ src/lib/types/generic.js
@@ -1,13 +1,13 @@
 import getSafeArray from "#src/util/array-util.js";
 
-function orderBy(p, type, key, direction) {
+function orderBy(p, type, key, variant) {
     return Object.entries(p[`${type}_usage`])
         .map(([ name, item ]) => { return { [name]: getSafeArray(item[key]).reduce( (total, v) => total + (v ?? 0), 0) } })
-        .sort((a, b) => { return (Object.values(direction === "descending" ? b : a)[0] - Object.values(direction === "descending" ? a : b)[0]); });
+        .sort((a, b) => { return (Object.values(variant === "descending" ? b : a)[0] - Object.values(variant === "descending" ? a : b)[0]); });
 }
 
-function getBy(p, type, key, direction, n = 10) {
-    return orderBy(p, type, key, direction).filter( (item, i) => i < n);
+function getBy(p, type, key, variant, n = 10) {
+    return orderBy(p, type, key, variant).filter( (item, i) => i < n);
 }
 
 export default getBy;
--- src/lib/types/jokers.js
+++ src/lib/types/jokers.js
@@ -1,7 +1,7 @@
 import getBy from "./generic.js"
 
-function getJokersBy(p, key, direction = "descending", n = 10) {
-    return getBy(p, "joker", key, direction, n);
+function getJokersBy(p, key, variant = "descending", n = 10) {
+    return getBy(p, "joker", key, variant, n);
 }
 
 export default getJokersBy;
