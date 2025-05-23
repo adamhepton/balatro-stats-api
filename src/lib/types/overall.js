d2f8f6668d62dd0788e4f93422265c66410d8890
Adam Hepton <adam@hepton.org>
2025-05-23 19:50:16 +0100

    Fixed CompletionPlusPlus to actually report the correct thing against progress 😬

--- src/lib/types/overall.js
+++ src/lib/types/overall.js
@@ -10,7 +10,8 @@ function getWinsBy(p, type, variant, atLeast) {
         .reduce((allWins, winsBy) => {
             allWins[winsBy.name] = {
                 detail: winsBy.winsByStake,
-                unique: winsBy.winsByStake.reduce((total, wins) => total += wins >= 1 ? 1 : 0, 0)
+                unique: winsBy.winsByStake.reduce((total, wins) => total += wins >= 1 ? 1 : 0, 0),
+                highestStakeWin: getHighestWinningStake(winsBy.winsByStake)
             }
             return allWins
         }, {})
@@ -18,13 +19,21 @@ function getWinsBy(p, type, variant, atLeast) {
     switch(variant) {
         case "summary":
         default:
-            return Object.values(wins)
-                .map(deckOrJokerWins => deckOrJokerWins.unique)
-                .reduce((total, val) => { total["tally"] += val; total["of"] += 8; return total }, { tally: 0, of: 0 })
+            switch(type) {
+                case "deck":
+                    return Object.values(wins)
+                        .map(deckWins => deckWins.unique)
+                        .reduce((total, val) => { total["tally"] += val; total["of"] += 8; return total }, { tally: 0, of: 0 })
+                
+                case "joker":
+                    return Object.values(wins)
+                        .map(deckWins => deckWins.highestStakeWin)
+                        .reduce((total, val) => { total["tally"] += (val === 7); total["of"] += 1; return total }, { tally: 0, of: 0 })
+            }
 
         case "byStake":
             return Object.entries(wins)
-                .map(([ name, item ]) => { return { name, highestStakeWin: getHighestWinningStake(item.detail) } })
+                .map(([ name, item ]) => { return { name, highestStakeWin: item.highestStakeWin } })
                 .sort((a, b) => b.highestStakeWin - a.highestStakeWin)
                 .filter(item => item.highestStakeWin >= atLeast)
         
