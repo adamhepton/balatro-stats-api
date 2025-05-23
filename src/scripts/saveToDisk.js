af32b3ffc4a3a88aadb96ad8549c5059b8fc7283
Adam Hepton <adam@hepton.org>
2025-05-23 12:17:53 +0100

    Updated paths for conversion; added as "convert" task

--- src/scripts/saveToDisk.js
+++ src/scripts/saveToDisk.js
@@ -2,6 +2,7 @@ import readBalatroFile from "#src/util/file-util.js";
 import fs from "fs";
 
 ["profile"].forEach(target => {
-    const json = readBalatroFile(`#data/${target}.jkr`, "json");
-    fs.writeFileSync(`game-files/${target}.json`, json);
+    const json = readBalatroFile(`./data/${target}.jkr`, "json");
+    fs.writeFileSync(`./data/${target}.json`, json);
+    console.log(`Written ${target}.json.`)
 });
