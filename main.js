020db3d637fd658ee576ec52c34cf98afc3da55d
Adam Hepton <adam@hepton.org>
2025-05-29 14:22:14 +0100

    Fixed default parameters to overall CompleteCompletionist

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import balatroStats from "#src/lib/balatroStats.js";
 import p from "#data/profile.json"  with { type: "json" };
 
-const [ type, query, variant, atLeast ] = process.argv.slice(2, 6) ?? ["jokers", "count", "descending", 0];
+function getFromArgvOrDefault(i) {
+    const defaults = ["overall", "CompleteCompletionist", "descending", 0];
+    return process.argv.at(i + 2) ?? defaults.at(i);
+}
+
+const [ type, query, variant, atLeast ] = [ getFromArgvOrDefault(0), getFromArgvOrDefault(1), getFromArgvOrDefault(2), getFromArgvOrDefault(3) ];
 console.log(balatroStats[type].call(this, p, query, variant, atLeast));
