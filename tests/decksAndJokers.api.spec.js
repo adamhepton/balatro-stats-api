e39f8a5a00c5d178ff8e96109343840e9b2d42e3
Adam Hepton <adam@hepton.org>
2025-08-10 20:36:31 +0100

    Refactor decksAndJokers tests

--- tests/decksAndJokers.api.spec.js
+++ tests/decksAndJokers.api.spec.js
@@ -36,65 +36,74 @@ test.group('Decks and Jokers API', () => {
   });
 
   test('fully ignores default parameters if all parameters passed', ({ assert }) => {
-    const usingPartialParameters = decksAndJokers(p, amendedParameters);
-    const expectedParametersCalled = decksAndJokers(p, {...expectedDefaults, ...amendedParameters});
-    assert.deepEqual(usingPartialParameters, expectedParametersCalled)
+    assert.deepEqual(
+      decksAndJokers(p, amendedParameters),
+      decksAndJokers(p, {...expectedDefaults, ...amendedParameters})
+    )
   });
 
   test('partially applies default parameters if type is missing', ({ assert }) => {
     const partialParameters = filterObject(amendedParameters, (k, v) => k !== "type");
-    const usingPartialParameters = decksAndJokers(p, partialParameters);
-    const expectedParametersCalled = decksAndJokers(p, {...expectedDefaults, ...partialParameters});
-    assert.deepEqual(usingPartialParameters, expectedParametersCalled)
+    assert.deepEqual(
+      decksAndJokers(p, partialParameters),
+      decksAndJokers(p, {...expectedDefaults, ...partialParameters})
+    )
   });
 
   test('partially applies default parameters if key is missing', ({ assert }) => {
     const partialParameters = filterObject(amendedParameters, (k, v) => k !== "key");
-    const usingPartialParameters = decksAndJokers(p, partialParameters);
-    const expectedParametersCalled = decksAndJokers(p, {...expectedDefaults, ...partialParameters});
-    assert.deepEqual(usingPartialParameters, expectedParametersCalled)
+    assert.deepEqual(
+      decksAndJokers(p, partialParameters),
+      decksAndJokers(p, {...expectedDefaults, ...partialParameters})
+    )
   });
 
   test('partially applies default parameters if variant is missing', ({ assert }) => {
     const partialParameters = filterObject(amendedParameters, (k, v) => k !== "variant");
-    const usingPartialParameters = decksAndJokers(p, partialParameters);
-    const expectedParametersCalled = decksAndJokers(p, {...expectedDefaults, ...partialParameters});
-    assert.deepEqual(usingPartialParameters, expectedParametersCalled)
+    assert.deepEqual(
+      decksAndJokers(p, partialParameters),
+      decksAndJokers(p, {...expectedDefaults, ...partialParameters})
+    )
   });
 
   test('partially applies default parameters if n is missing', ({ assert }) => {
     const partialParameters = filterObject(amendedParameters, (k, v) => k !== "n");
-    const usingPartialParameters = decksAndJokers(p, partialParameters);
-    const expectedParametersCalled = decksAndJokers(p, {...expectedDefaults, ...partialParameters});
-    assert.deepEqual(usingPartialParameters, expectedParametersCalled)
+    assert.deepEqual(
+      decksAndJokers(p, partialParameters),
+      decksAndJokers(p, {...expectedDefaults, ...partialParameters})
+    )
   });
 
   test('partially applies default parameters if only type is passed', ({ assert }) => {
     const partialParameters = filterObject(amendedParameters, (k, v) => k === "type");
-    const usingPartialParameters = decksAndJokers(p, partialParameters);
-    const expectedParametersCalled = decksAndJokers(p, {...expectedDefaults, ...partialParameters});
-    assert.deepEqual(usingPartialParameters, expectedParametersCalled)
+    assert.deepEqual(
+      decksAndJokers(p, partialParameters),
+      decksAndJokers(p, {...expectedDefaults, ...partialParameters})
+    )
   });
 
   test('partially applies default parameters if only key is passed', ({ assert }) => {
     const partialParameters = filterObject(amendedParameters, (k, v) => k === "key");
-    const usingPartialParameters = decksAndJokers(p, partialParameters);
-    const expectedParametersCalled = decksAndJokers(p, {...expectedDefaults, ...partialParameters});
-    assert.deepEqual(usingPartialParameters, expectedParametersCalled)
+    assert.deepEqual(
+      decksAndJokers(p, partialParameters),
+      decksAndJokers(p, {...expectedDefaults, ...partialParameters})
+    )
   });
 
   test('partially applies default parameters if only variant is passed', ({ assert }) => {
     const partialParameters = filterObject(amendedParameters, (k, v) => k === "variant");
-    const usingPartialParameters = decksAndJokers(p, partialParameters);
-    const expectedParametersCalled = decksAndJokers(p, {...expectedDefaults, ...partialParameters});
-    assert.deepEqual(usingPartialParameters, expectedParametersCalled)
+    assert.deepEqual(
+      decksAndJokers(p, partialParameters),
+      decksAndJokers(p, {...expectedDefaults, ...partialParameters})
+    )
   });
 
   test('partially applies default parameters if only n is passed', ({ assert }) => {
     const partialParameters = filterObject(amendedParameters, (k, v) => k === "n");
-    const usingPartialParameters = decksAndJokers(p, partialParameters);
-    const expectedParametersCalled = decksAndJokers(p, {...expectedDefaults, ...partialParameters});
-    assert.deepEqual(usingPartialParameters, expectedParametersCalled)
+    assert.deepEqual(
+      decksAndJokers(p, partialParameters),
+      decksAndJokers(p, {...expectedDefaults, ...partialParameters})
+    )
   });
 
   test('throws an exception if non-supported type is requested', ({ assert }) => {
